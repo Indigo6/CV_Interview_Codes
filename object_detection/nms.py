ce408fd5f7c53432e7fb62244a21bacc865bd4e4
Indigo6 <1401322857@qq.com>
2022-08-04 14:37:54 +0800

    update nms.py

--- object_detection/nms.py
+++ object_detection/nms.py
@@ -3,43 +3,31 @@ import numpy as np
 def nms(bboxes, thresh=0.5):
     """NMS: Non-maximum Suppression
         Args:
-            bboxes: [N,4], torch.Tensor
-            scores: [N,1], torch.Tensor
+            bboxes: bboxes[N,:4], torch.Tensor
+            scores: bboxes[N, 4], torch.Tensor
     """
-    x1 = bboxes[:, 0] # xmin
-    y1 = bboxes[:, 1] # ymin
-    x2 = bboxes[:, 2] # xmax
-    y2 = bboxes[:, 3] # ymax
-    scores = bboxes[:, 4] # score
-    
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1) # 每个boundingbox的面积
-    order = scores.argsort()[::-1] # boundingbox的置信度排序
-    keep = [] # 用来保存最后留下来的boundingbox
-    while True:     
+    areas = ((bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1]))
+    scores = bboxes[:, -1]
+    lts = bboxes[:, :2]
+    rbs = bboxes[:, 2:4]
+    order = np.argsort(scores)[::-1]
+
+    keep = []
+    while True:
         if len(order)<=1:
             keep.extend(order)
             break
-        i = order[0] # 置信度最高的boundingbox的index
-        keep.append(i) # 添加本次置信度最高的boundingbox的index
+        i = order[0]
+        keep.append(i)
+
+        inter_lt = np.maximum(lts[i], lts[order[1:]])
+        inter_rb = np.minimum(rbs[i], rbs[order[1:]])
+        inter_wh = np.maximum(inter_rb - inter_lt, 0)
+        inter_area = inter_wh[:, 0] * inter_wh[:, 1]
+        ious = inter_area / (areas[i] + areas[order[1:]] - inter_area)
         
-        # 当前bbox和剩下bbox之间的交叉区域
-        # 选择大于x1,y1和小于x2,y2的区域
-        xx1 = np.maximum(x1[i], x1[order[1:]]) #交叉区域的左上角的横坐标
-        yy1 = np.maximum(y1[i], y1[order[1:]]) #交叉区域的左上角的纵坐标
-        xx2 = np.minimum(x2[i], x2[order[1:]]) #交叉区域右下角的横坐标
-        yy2 = np.minimum(y2[i], y2[order[1:]]) #交叉区域右下角的纵坐标
-        
-        # 当前bbox和其他剩下bbox之间交叉区域的面积
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
-        
-        # 交叉区域面积 / (bbox + 某区域面积 - 交叉区域面积)
-        ious = inter / (areas[i] + areas[order[1:]] - inter)
-        #保留交集小于一定阈值的boundingbox
-        inds = np.where(ious <= thresh)[0]
-        order = order[inds + 1]
-        
+        indexes = np.where(ious<thresh)[0]
+        order = order[indexes+1]
     return keep
 
 def soft_nms(boxes, sigma=0.5, threshold1=0.7, threshold2=0.4, method=1):
