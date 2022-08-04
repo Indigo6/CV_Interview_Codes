3e6e1e93e7f6a430aea61e1a26186a13174e393e
Indigo6 <1401322857@qq.com>
2022-08-04 16:33:09 +0800

    add image_processing

---
+++ image_processing/filter.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def gaussian_filter(image, kernel_size=7, sigma=2):
+    """
+        image: [h, w, 3]
+    """
+    h, w, _ = image.shape
+
+    # pad
+    radius = kernel_size // 2
+    padded_image = np.zeros(([h+2*radius, w+2*radius, 3]))
+    padded_image[3:-3, 3:-3] = image
+
+    # generate filter
+    # filter = np.zeros([kernel_size, kernel_size])
+    indexes = np.linspace(0, kernel_size-1, kernel_size)
+    x = indexes[:, np.newaxis]
+    y = indexes[np.newaxis, :]
+    filter = np.exp(- ((x-radius)**2+(y-radius)**2) / sigma**2)
+    filter = filter[:, :, np.newaxis]
+
+    # perform filter
+    for i in range(h):
+        for j in range(w):
+            image[i][j][:] = np.sum(padded_image[i:i+2*radius+1, j:j+2*radius+1, :] * filter)
+
+    return image
+
+if __name__ == "__main__":
+    test_image = np.ones([3, 3, 3])
+    print(gaussian_filter(test_image)[:, :, 0])
