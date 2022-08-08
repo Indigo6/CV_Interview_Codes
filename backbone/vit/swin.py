a0ec83e7b6fd6325f82abe6361b0b733dcb4535c
Indigo6 <1401322857@qq.com>
2022-08-08 15:22:22 +0800

    add swin.py

---
+++ backbone/vit/swin.py
@@ -0,0 +1,47 @@
+from turtle import forward
+import torch
+
+from torch import nn
+from vit import Patch_Embedding, MLP
+
+
+class SwinBlock(nn.Module):
+    def __init__(self, embed_dim) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+
+class SwinLayer(nn.Module):
+    def __init__(self, embed_dim) -> None:
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        self.atten = SwinBlock(embed_dim)
+        self.atten_norm = nn.LayerNorm(embed_dim)
+
+        self.ffn = nn.Linear(embed_dim, embed_dim)
+        self.ffn_norm = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        h = x
+        x = self.atten_norm(x)
+        x = self.atten(x)
+        x = x + h
+        
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+
+        return x
+
+
+class SwinTransformer(nn.Module):
+    def __init__(self, patch_size, embed_dim) -> None:
+        super().__init__()
+        self.patch_embed = Patch_Embedding(patch_size, 3, embed_dim)
+
+    def forward(self, x):
+        x = self.patch_embed(x)
